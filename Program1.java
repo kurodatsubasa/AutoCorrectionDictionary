c95252a46b6e96f95492277c845568df9f871380
Hai Le <35050699+kurodatsubasa@users.noreply.github.com>
2018-01-16 16:02:40 -0600

    Final Version of Program1
    Setup the user prompt and dictionary search.

--- Program1.java
+++ Program1.java
@@ -8,35 +8,43 @@
  */
 package program1;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Program1 {
 
     public static void main(String[] args) throws Exception {
+        // initializes dicitionary
         Dictionary dictionary = new Dictionary();
+        TextMessage message = new TextMessage();
+        // reading list of words
         FileIO.readFile("words.txt", dictionary, 0);
+        // reading-in keyboard layout
         FileIO.readFile("keyboard.txt", dictionary, 1);
         
-        // Enter a text message, "done" (no quotes) to exit: 
+        // sets scanner to read the user input from kb
         Scanner sc = new Scanner(System.in);
         
+        // to store user input
         String input;
         
-        System.out.println("Enter a phrase to check, type 'done' to exit:");
+        // prompts the user for input and stores in String input
+        System.out.println("Enter a text message, \"done\" (no quotes) to exit");
         input = sc.nextLine();
         
-        
-        while (input.equalsIgnoreCase("done"))
-        {      
-            System.out.println();
-            TextMessage.textMessage(input);
-
-            System.out.println();
-            System.out.println("Enter a phrase to check, type 'done' to exit:");
+        // runs the program until the user enters "done" which sets the flag to true
+        while (!input.equalsIgnoreCase("done")) {    
+            // check if input is empty before processing it
+            if(!input.isEmpty()) {
+                // splist up the user input into proper words for processing 
+                message.setInputStr(input);
+                // searches the dictionary for proper words in the message
+                dictionary.search(message.getWords());
+                // clears the content of message before the next user input
+                message.clear();
+            } // end if
+            // prompts the user for input and stores in String input
+            System.out.println("\nEnter a text message, \"done\" (no quotes) to exit");
             input = sc.nextLine();   
-            
-        }
-        
-    }
-    
-}
+        } // end while
+    } // end main
+} // end Program 1
