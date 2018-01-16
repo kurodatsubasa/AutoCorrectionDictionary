620d1992f86c8beeef84bfa55523f9b7f0626ba6
Art <oceguera.arturo@gmail.com>
2018-01-15 22:09:01 -0600

    user input

--- Program1.java
+++ Program1.java
@@ -8,12 +8,35 @@
  */
 package program1;
 
+import java.util.*;
+
 public class Program1 {
 
     public static void main(String[] args) throws Exception {
         Dictionary dictionary = new Dictionary();
         FileIO.readFile("words.txt", dictionary, 0);
         FileIO.readFile("keyboard.txt", dictionary, 1);
+        
+        // Enter a text message, "done" (no quotes) to exit: 
+        Scanner sc = new Scanner(System.in);
+        
+        String input;
+        
+        System.out.println("Enter a phrase to check, type 'done' to exit:");
+        input = sc.nextLine();
+        
+        
+        while (input.equalsIgnoreCase("done"))
+        {      
+            System.out.println();
+            TextMessage.textMessage(input);
+
+            System.out.println();
+            System.out.println("Enter a phrase to check, type 'done' to exit:");
+            input = sc.nextLine();   
+            
+        }
+        
     }
     
 }
